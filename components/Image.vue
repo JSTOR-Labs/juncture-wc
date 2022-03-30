87f0c442b7d6966aeb087aedacd592616c51f2fa
Ron Snyder <ron@snyderjr.com>
2022-03-30 19:59:18 -0400

    testing

--- components/Image.vue
+++ components/Image.vue
@@ -1,7 +1,13 @@
 <template>
   <div :style="containerStyle">
 
-    <ve-image src="wc:Boy_and_book_by_Charles_Porter.jpg"></ve-image>
+    <ve-image src="wc:Boy_and_book_by_Charles_Porter.jpg">
+      <ul>
+        <li v-for="item, idx in viewerItems" :key="idx">
+          {{item.url}}
+        </li>
+      </ul>
+    </ve-image>
 
   </div>  
 </template>
@@ -9,7 +15,7 @@
 <script>
 
 module.exports = {
-  name: 've-image-v2',
+  name: 've1-image',
   props: {
     items: { type: Array, default: () => ([]) },
     viewerIsActive: Boolean
@@ -20,12 +26,13 @@ module.exports = {
     dependencies: []
   }),
   computed: {
-    containerStyle() { return { height: this.viewerIsActive ? '100%' : '0' } }
+    containerStyle() { return { height: this.viewerIsActive ? '100%' : '0' } },
+    viewerItems() { return this.items.filter(item => item.viewer === this.$options.name) },
   },
   mounted() { this.loadDependencies(this.dependencies, 0, this.init) },
   methods: {
     init() {
-      console.log(`${this.$options.name}.mounted`, this.items)
+      console.log(`${this.$options.name}.mounted`, this.items, this.viewerItems)
     }
   }
 }
