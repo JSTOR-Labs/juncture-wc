8577ae949c5a9ac8609cf7548110c57e4af8aaf1
Ron Snyder <ron@snyderjr.com>
2022-04-20 06:48:52 -0400

    Added compare component

---
+++ components/Compare.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :style="containerStyle">
+
+    <ve-image :compare="mode">
+      <ul>
+        <li v-for="(image, idx) in images" :key="idx">{{image.manifest}}{{image.options ? ' '+image.options : ''}}</li>
+      </ul>
+    </ve-image>
+
+  </div>  
+</template>
+
+<script>
+
+module.exports = {
+  name: 've2-image',
+  props: {
+    items: { type: Array, default: () => ([]) },
+    viewerIsActive: Boolean
+  },
+  data: () => ({
+    viewerLabel: 'Image Compare',
+    viewerIcon: 'fas fa-images',
+    dependencies: []
+  }),
+  computed: {
+    containerStyle() { return { height: this.viewerIsActive ? '100%' : '0' } },
+    images() { return this.items.filter(item => item.viewer === 've-compare-v2') },
+    images() { return this.viewerItems },
+    mode() { let firstItemWithMode = this.images.find(item => item.compare)
+             return firstItemWithMode ? firstItemWithMode.compare : 'curtain'
+    },
+  }
+}
+
+</script>
+
+<style>
+</style>
--- components/Image.vue
+++ components/Image.vue
@@ -19,31 +19,14 @@ module.exports = {
     viewerIsActive: Boolean
   },
   data: () => ({
-    viewerLabel: 'Image Web Component',
-    viewerIcon: 'fas fa-code',
+    viewerLabel: 'Image Viewer',
+    viewerIcon: 'fas fa-file-image',
     dependencies: []
   }),
   computed: {
     containerStyle() { return { height: this.viewerIsActive ? '100%' : '0' } },
     viewerItems() { return this.items.filter(item => item.viewer === 've-image') },
-    manifestUrls() {
-      return this.viewerItems.map(item => {
-        return item.manifest || item.src
-          ? item.manifest || item.src
-          : `/${item.url}`
-      })
-    }
-  },
-  mounted() { this.loadDependencies(this.dependencies, 0, this.init) },
-  methods: {
-    init() {
-      console.log(`${this.$options.name}.mounted`, this.items, this.viewerItems)
-    }
-  },
-  watch: {
-    manifestUrls (current) {
-      console.log('watch.manifestUrls', current)
-    }
+    manifestUrls() { return this.viewerItems.map(item => item.manifest || item.src ? item.manifest || item.src : `/${item.url}`) }
   }
 }
 
